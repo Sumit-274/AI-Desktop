5c86b49226340942f0d29d0bdf3698a4ca7434d4
Sumit <sumitghotane274@gmail.com>
2021-04-21 14:38:15 +0530

    I Have Succesfully created the Verification System of Jarvis

---
+++ Program.py
@@ -0,0 +1,17 @@
+import speech_recognition as sr
+import pyttsx3 as pt3
+import webbrowser as brower
+import os
+
+def Login(password):
+    exact_password = 789
+    if password == exact_password:
+        print("Your Password is correct")
+        while True:
+            pass
+    else:
+        return "Your Password is incorrect"
+        exit()
+
+inp = int(input())
+Login(inp)
