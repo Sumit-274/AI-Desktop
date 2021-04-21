280a888570061dad9168147898d09cd63a92feb7
Sumit <sumitghotane274@gmail.com>
2021-04-21 15:43:00 +0530

    Speaking Functionality has been added

--- Program.py
+++ Program.py
@@ -7,12 +7,17 @@ def Login(password):
     exact_password = 789
     if password == exact_password:
         print("Your Password is correct")
-        while True:
-            pass
     else:
         return "Your Parpssword is incorrect"
         exit()
 
+def Speak(string):
+    engine = pt3.init()
+    word = string.lower()
+    engine.say(word)
+    engine.runAndWait()
+    
 if __name__ == "__main__": 
     inp = int(input())
     Login(inp)
+    Speak("My name is sumit")
